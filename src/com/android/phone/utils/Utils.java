b9807e533fad0b224916098a07fbbba7991573e1
Qiang Chen <quic_cqiang@quicinc.com>
2024-06-11 14:38:31 +0530

    TeleService: Adapt edge-to-edge enforcement
    Android 15 enables edge-to-edge by default.Phone Info should do the
    adaptation, or not some UIs may be obscured by system bars.
    
    Apply window insets for radio info activity content layout.
    
    Change-Id: I588c298f0c240dbc5c47e77db4a5689b20dc9c37
    CRs-Fixed: 3807596

---
+++ src/com/android/phone/utils/Utils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+package com.android.phone.utils;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Utils {
+
+    /**
+     * Enable new edge to edge feature.
+     *
+     * @param activity the Activity need to setup the edge to edge feature.
+     */
+    public static void setupEdgeToEdge(@NonNull Activity activity) {
+        ViewCompat.setOnApplyWindowInsetsListener(activity.findViewById(android.R.id.content),
+                (v, windowInsets) -> {
+                    Insets insets = windowInsets.getInsets(
+                            WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.ime());
+
+                    // Apply the insets paddings to the view.
+                    v.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+
+                    // Return CONSUMED if you don't want the window insets to keep being
+                    // passed down to descendant views.
+                    return WindowInsetsCompat.CONSUMED;
+                });
+    }
+}
