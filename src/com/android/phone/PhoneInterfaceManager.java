7f16a7b72ffcf903ca226edfe91c6b2dbfb63a61
Hyosun Kim <hyosunkim@google.com>
2024-07-05 02:44:27 +0000

    Add requestProvisionSubscriberIds , requestIsSatelliteProvisioned and provisionSatellite for test
    Bug: 351278182
    Test: atest SatelliteManagerTestOnMockService
    Test: manual test with SatelliteTestApp b/351278182#comment2
    verify requestProvisionSubscriberIds, requestIsSatelliteProvisioned and provisionSatellite
    Flag: com.android.internal.telephony.flags.carrier_roaming_nb_iot_ntn
    
    Change-Id: Ifbe45875a77178f1307371949cca147b2e5fffe0

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -14309,7 +14309,8 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      * @throws SecurityException if the caller doesn't have the required permission.
      */
     @Override
-    public void requestIsProvisioned(String satelliteSubscriberId, @NonNull ResultReceiver result) {
+    public void requestIsProvisioned(@NonNull String satelliteSubscriberId,
+            @NonNull ResultReceiver result) {
         enforceSatelliteCommunicationPermission("requestIsProvisioned");
         mSatelliteController.requestIsProvisioned(satelliteSubscriberId, result);
     }
@@ -14323,7 +14324,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      * @throws SecurityException if the caller doesn't have the required permission.
      */
     @Override
-    public void provisionSatellite(List<ProvisionSubscriberId> list,
+    public void provisionSatellite(@NonNull List<ProvisionSubscriberId> list,
             @NonNull ResultReceiver result) {
         enforceSatelliteCommunicationPermission("provisionSatellite");
         mSatelliteController.provisionSatellite(list, result);
